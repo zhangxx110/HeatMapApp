5d1f67518f1ed75eb9d093dabf5ed6d9ad0be3dc
zhangxiongxiang <zhangxiongxiang@didichuxing.com>
2016-12-14 17:46:00 +0800

    添加文件

---
+++ js/revertAddress.js
@@ -0,0 +1,3 @@
+/**
+ * Created by zhangxiongxiang on 16/12/11.
+ */
---
+++ obj/HeatAddressObj.js
@@ -0,0 +1,11 @@
+/**
+ * Created by zhangxiongxiang on 16/12/11.
+ */
+
+var creatHeatMapObj = function (lng,lat,count){
+    var obj = new Object();
+    obj.lng = lng;
+    obj.lat = lat;
+    obj.count = count;
+    return obj;
+}
